69e71b5cdb73680b8a2615f837029626464b87e3
Ubuntu <Darian@Yojan.fxtpsoe5a30ebawzyl232dp1jd.bx.internal.cloudapp.net>
2018-12-22 15:16:45 +0000

    servidor modificado linux

--- dist/my-duver/server.js
+++ dist/my-duver/server.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const http = require('http');
-const path = require('path');
-
-const app = express();
-
-const port = process.env.PORT || 4200;
-
-app.use(express.static(path.join(__dirname,'')));
-
-app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'')));
-
-const server = http.createServer(app);
-
-server.listen(port,()=> console.log('corriendo'));
-=======
 const express = require ('express');
 const http = require ('http');
 const path = require ('path');
@@ -30,4 +13,3 @@ app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'index.html')));
 const server = http.createServer(app);
 
 server.listen(port,()=> console.log('corriendo'));
->>>>>>> 6c50ebc0495d556bbb74d1471537ea88f7668f23
